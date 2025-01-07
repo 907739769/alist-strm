e3e6f129660ceec7d9310a0abf64d8d8b109a783
DDSDerek <108336573+DDSDerek@users.noreply.github.com>
2025-01-07 19:08:26 +0800

    fix: could not find or load main class aliststrm.jar

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ cd /app
 
 umask "${UMASK}"
 
-exec gosu "${PUID}":"${PGID}" java $JAVA_OPTS -XX:+UseG1GC -XX:+OptimizeStringConcat -XX:+PrintGCDetails -Xloggc:/log/gc.log -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCApplicationConcurrentTime -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/log aliststrm.jar
+exec gosu "${PUID}":"${PGID}" java $JAVA_OPTS -XX:+UseG1GC -XX:+OptimizeStringConcat -XX:+PrintGCDetails -Xloggc:/log/gc.log -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCApplicationConcurrentTime -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/log -jar /app/aliststrm.jar
